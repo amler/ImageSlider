5129b3985b0ccc767e7a8b7b0c80aaa94aa12f7b
Heather Amler <heatheramler@yahoo.com>
2014-05-28 22:42:22 -0400

    Test rewrite for Object constructor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,17 +4,47 @@ var myImageArray = [
 	"http://goo.gl/B6Yg7e"
 ];
 
-// keep track of which image we're on
-var current = 0;
 
-function createSlideshow () {
+function Slideshow (array) {
+	this.renderImage = function () {
+							myImageArray.forEach(function (photo) {
+	    						$('.slides').append('<img class = "pics" src=' + photo  + ' />');
+							});
+						}
+
+};
+
+var arrayValidator = function(array){
+	if (!$.isArray(array)) {
+		throw new Error ('Not an array!');
+	} else if (array.length <= 0) {
+		throw new Error ('Empty image array.');
+	}
+	else {
+		var imageSlider = new Slideshow(array);
+	}
+}
+arrayValidator(myImageArray);
+
+
+
+
+/*function createSlideshow () {
 	
 	myImageArray.forEach(function (photo) {
-	    $('.slides').append('<img class = "pics" src=' + photo  + ' />')
+	    $('.slides').append('<img class = "pics" src=' + photo  + ' />');
 	});
 	// after rendering set interval
-	var myVar = setInterval(function(){slidePic()}, 1000);
-}
+	//var myVar = setInterval(function(){slidePic()}, 1000);
+}*/
+
+
+/*var imagegGallery = new Slideshow*/
+
+/*
+var current = 0;
+
+
 
 createSlideshow();
 
@@ -25,10 +55,9 @@ function slidePic () {
 	if (current>= myImageArray.length) {
 		current = 0;
 	}
-	
+	// move back -400 for current 
 	var position = current * -400;
 
 	$('.slides').animate({marginLeft: position}, 500);	
 }
-
-
+*/
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,41 +1,118 @@
 /* global describe, it */
 
 (function () {
-    'use strict';
-    		//describe the name of the function aka name
-    describe('Image Slider', function () {
-        describe('Image slider with images', function () {
-            
-            it('should inject the correct number of image tags', function () {
-            	createSlideshow(myImageArray);
-            	var total = $('.slides .pics').length;
-            	console.log(total);
-            	expect(total).to.equal(myImageArray.length);
-            });
-        });
+	'use strict';
+
+    describe("imgGenerator()  function", function(){
+        describe('Should validate array and render images', function () {
+        	var myImageArray = [
+				"http://goo.gl/X88VdC",
+				"http://goo.gl/vjMK2c",
+				"http://goo.gl/B6Yg7e"
+			];
+
+	      	it("should only accept an array for images as an argument.", function(){
+	        	var emptyArray = function () {
+	        		slideshow([]);
+	        	}
+	        	expect(emptyArray).to.throw(Error)
+	      	});
+
+	      	it("should not accept an empty array as an argument.", function(){
+	        	var stringArray = function () {
+	        		slideshow('string');
+	        	}
+	        	expect(stringArray).to.throw(Error)
+	      	});
+
+	      	it("should only contain 3 images - in case client specific.", function(){
+	      	
+	        	expect(myImageArray.length).to.equal(3)
+
+	      	});
+
+	      	it("should assert that the target is an instance of constructor.", function(){
+        
+				var imageSlider = new Slideshow(myImageArray);
+				expect(imageSlider).to.be.an.instanceof(Slideshow);
+			});
+	      	/*
+	      	it("should confirm that each array index has a jpeg, jpg, gif, or png extensions.", function(){
+				//not sure how to test for it or how to write it.
+				var confirmExt = function () {
+					slideshow([rfwerf.jpg, rfref.gif])
+				}
+				expect()
+	      	});
+			*/
+	    });
     });
 
-    describe('Image Interval', function () {
-        describe('each image is rendered', function () {
-            it('should display each image in a set time', function (done) {
+	describe("Slideshow()", function(){
+        describe('Slideshow constructor should have methods', function () {
+
+	      	it("should accept an array as an argument and render each image to the DOM.", function(){
+	      		forEac
+	      	});
+	
+})();
+
+
+/*it("should assert that the target is an instance of constructor.", function(){
+
+		        var imagegallery = new Slideshow();
+		        expect(imagegallery).to.be.an.instanceof(Slideshow);
+	      	});*/
+
+
+  /*  describe('Create Slideshow', function () {
+            
+        it('should inject the correct number of image tags', function () {
+        	var imageCount = myImageArray.length;
+        	var total = $('.slides .pics').length;
+        	console.log(total);
+        	expect(total).to.equal(imageCount);
+        });
+   describe('Slide Pic', function () {
+        describe('Images slideshow ', function () {
+            it('Images should match the array as the slideshow progresses', function (done) {
 				setTimeout( function () {
 					slidePic( done, function() {
 						expect(true).to.equal(true);
 					} )
 				}, 3000);
+				// call function
+				slidePic();
+
 
             });
         });
-    });
+    });*/
 	
 
-	describe('Validate array', function () {
-	        describe('validate data', function () {
-	            it('should make sure its a string of urls', function () {
-	            	expect(myImageArray).to.have.property('http');
+	/*describe('some meaningful topic', function () {
+	            it('some specific thing that it should do (maybe in reaction something)', function () {
+	            	expect(YOUTRYTHETHINGANDTHISWHATITACTUALLYDID).to.have.property(WHATYOUKNOWITSHOULDDO);
+	            });
+
+	            it('should do some specific thing thats asynchronous', function () {
+	            	this.timeout(5000);
+	            	setTimeout( function () {
+		            	expect(YOUTRYTHETHINGANDTHISWHATITACTUALLYDID).to.equal(WHATYOUKNOWITSHOULDDO);
+						done()
+					} )
+				}, 3000);
+
+	            });
+
+	            it('should throw an error when bad things happen', function () {
+	            	var badFunctionReference = function () {
+	            		myFunction (TERRIBLEARGUMENT);
+	            	}
+	            	expect(AREFERENCETOAFUNCTIONTHATCONTAINSANOTHERBADFUNCTIONCALL).to.throw(ERROR);
 	            });
 	        });
 	    });
+*/
 
 
-})();
